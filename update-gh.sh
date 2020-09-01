e73779b55e26921025535226cbed904790423aef
Axmouth <aggronfg@yahoo.com>
2020-09-01 03:57:46 +0300

    Didn't fix the url issue it seems, but hopefully now.

--- update-gh.sh
+++ update-gh.sh
@@ -1,7 +1,7 @@
 cd postgrustql_wasm_client
 wasm-pack build
 cd ../postgrustql-ng-repl
-ng build --prod=true --base-href=https://axmouth.github.io/postgrustql/ --deployUrl=https://axmouth.github.io/postgrustql/
+ng build --prod=true --base-href=/postgrustql/ --deployUrl=/postgrustql/
 cd ..
 
 cp -r postgrustql-ng-repl/dist/postgrustql-ng-repl docs
