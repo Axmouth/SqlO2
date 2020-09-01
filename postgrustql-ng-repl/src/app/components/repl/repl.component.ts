ede0e847717a993fa68b098ba8964c107cdcafb0
Axmouth <aggronfg@yahoo.com>
2020-09-01 16:32:16 +0300

    Fixed an error in previous functionality.

--- postgrustql-ng-repl/src/app/components/repl/repl.component.ts
+++ postgrustql-ng-repl/src/app/components/repl/repl.component.ts
@@ -24,49 +24,46 @@ export class ReplComponent implements OnInit, OnDestroy {
   constructor(private postgrustqlService: PostgrustqlService, private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
-    this.router.events
-      .pipe(take(1))
-      .pipe(takeUntil(this.notifier))
-      .subscribe(async (event) => {
-        if (event instanceof NavigationEnd) {
-          // Get a good value
-          const params = this.route.snapshot.queryParams;
-          console.log(params);
+    this.router.events.pipe(takeUntil(this.notifier)).subscribe(async (event) => {
+      if (event instanceof NavigationEnd) {
+        // Get a good value
+        const params = this.route.snapshot.queryParams;
+        console.log(params);
 
-          if (Array.isArray(params.q)) {
-            for (const query of params.q) {
-              this.queryString = query;
-              await this.onQuerySubmit();
-            }
-          } else if (params.q === '') {
-            return;
-          } else if (params.q !== undefined) {
-            this.queryString = params.q;
-            await this.onQuerySubmit();
-          } else {
-            this.queryString = `CREATE TABLE regulars (id INT, name TEXT);`;
-            await this.onQuerySubmit();
-            this.queryString = `INSERT INTO regulars VALUES (1, '25th Baam');`;
-            await this.onQuerySubmit();
-            this.queryString = `INSERT INTO regulars VALUES (2, 'Rachel'); `;
-            await this.onQuerySubmit();
-            this.queryString = `INSERT INTO regulars VALUES (3, 'Rak WraithKaiser');`;
-            await this.onQuerySubmit();
-            this.queryString = `INSERT INTO regulars VALUES (4, 'Khun Aguero Agnes');`;
-            await this.onQuerySubmit();
-            this.queryString = `SELECT id, name FROM regulars;`;
-            await this.onQuerySubmit();
-            this.queryString = `SELECT id, name FROM regulars where id != 2;`;
-            await this.onQuerySubmit();
-            this.queryString = `SELECT id, name FROM regulars where name = 'Rachel'`;
-            await this.onQuerySubmit();
-            this.queryString = `SELECT id, name as charName FROM regulars where name = 'Rachel';`;
-            await this.onQuerySubmit();
-            this.queryString = `SELECT id, name as "regulars.charName" FROM regulars where name = 'Rachel';`;
+        if (Array.isArray(params.q)) {
+          for (const query of params.q) {
+            this.queryString = query;
             await this.onQuerySubmit();
           }
+        } else if (params.q === '') {
+          return;
+        } else if (params.q !== undefined) {
+          this.queryString = params.q;
+          await this.onQuerySubmit();
+        } else {
+          this.queryString = `CREATE TABLE regulars (id INT, name TEXT);`;
+          await this.onQuerySubmit();
+          this.queryString = `INSERT INTO regulars VALUES (1, '25th Baam');`;
+          await this.onQuerySubmit();
+          this.queryString = `INSERT INTO regulars VALUES (2, 'Rachel'); `;
+          await this.onQuerySubmit();
+          this.queryString = `INSERT INTO regulars VALUES (3, 'Rak WraithKaiser');`;
+          await this.onQuerySubmit();
+          this.queryString = `INSERT INTO regulars VALUES (4, 'Khun Aguero Agnes');`;
+          await this.onQuerySubmit();
+          this.queryString = `SELECT id, name FROM regulars;`;
+          await this.onQuerySubmit();
+          this.queryString = `SELECT id, name FROM regulars where id != 2;`;
+          await this.onQuerySubmit();
+          this.queryString = `SELECT id, name FROM regulars where name = 'Rachel'`;
+          await this.onQuerySubmit();
+          this.queryString = `SELECT id, name as charName FROM regulars where name = 'Rachel';`;
+          await this.onQuerySubmit();
+          this.queryString = `SELECT id, name as "regulars.charName" FROM regulars where name = 'Rachel';`;
+          await this.onQuerySubmit();
         }
-      });
+      }
+    });
   }
 
   async onQuerySubmit() {
