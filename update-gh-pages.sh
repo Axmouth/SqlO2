a7b1cc833436be5b0b6d364684bf5f0b18628203
Axmouth <aggronfg@yahoo.com>
2020-09-01 14:31:09 +0300

    Corrected oopsie where github pages material was copied a folder too deep

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -4,4 +4,4 @@ cd ../postgrustql-ng-repl
 ng build --prod=true --base-href=/postgrustql/ --deployUrl=/postgrustql/
 cd ..
 
-cp -r postgrustql-ng-repl/dist/postgrustql-ng-repl docs
+cp -r postgrustql-ng-repl/dist/postgrustql-ng-repl/ docs
