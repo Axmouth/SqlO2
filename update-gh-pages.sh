57312d0a1fd9cf058d7b5b2793620def4eca0312
Axmouth <aggronfg@yahoo.com>
2020-09-01 14:42:21 +0300

    a

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -4,4 +4,5 @@ cd ../postgrustql-ng-repl
 ng build --prod=true --base-href=https://axmouth.github.io/postgrustql/ --deployUrl=https://axmouth.github.io/postgrustql/
 cd ..
 
+mkdir -p docs
 cp -r postgrustql-ng-repl/dist/postgrustql-ng-repl/* docs
