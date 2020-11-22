df535a785e6906b60875346a676bd79de3b9383d
Axmouth <georgenikolop93@gmail.com>
2020-11-22 11:44:14 +0000

    Deploying to gh-pages from  @ 0d6320a7f6ab7ca15d343b51b3ae02ed5c008ba1 🚀

--- update-gh-pages.sh
+++
@@ -1,8 +0,0 @@
-cd postgrustql_wasm_client
-wasm-pack build
-cd ../postgrustql-ng-repl
-ng build --prod=true --base-href=https://axmouth.github.io/postgrustql/ --deployUrl=https://axmouth.github.io/postgrustql/
-cd ..
-
-mkdir -p docs
-cp -r postgrustql-ng-repl/dist/postgrustql-ng-repl/* docs
