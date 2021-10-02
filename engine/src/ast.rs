4c5a852096bbfe3614dfb23cbe5804818a4de8b5
Axmouth <aggronfg@yahoo.com>
2021-10-03 02:23:48 +0300

    Refactor many [] operations out

--- engine/src/ast.rs
+++ engine/src/ast.rs
@@ -368,7 +368,7 @@ impl Operand {
         .to_string()
     }
 
-    pub fn from_token(token: Token) -> Result<Operand, ParsingError> {
+    pub fn from_token(token: &Token) -> Result<Operand, ParsingError> {
         match token {
             Token::Plus => Ok(Operand::Add),
             Token::Minus => Ok(Operand::Subtract),
@@ -453,103 +453,7 @@ impl UnaryExpression {
 
 impl Token<'_> {
     pub fn generate_code(&self) -> String {
-        match self {
-            Token::And => "AND".to_string(),
-            Token::As => "AS".to_string(),
-            Token::Asterisk => "*".to_string(),
-            Token::Bool => "BOOL".to_string(),
-            Token::BoolValue { value } => {
-                if *value {
-                    "TRUE".to_string()
-                } else {
-                    "FALSE".to_string()
-                }
-            }
-            Token::Comma => COMMA_SYMBOL.to_string(),
-            Token::Concat => CONCAT_SYMBOL.to_string(),
-            Token::Create => CREATE_KEYWORD.to_string(),
-            Token::Drop => DROP_KEYWORD.to_string(),
-            Token::Empty => "".to_string(),
-            Token::Equal => EQUAL_SYMBOL.to_string(),
-            Token::False => FALSE_KEYWORD.to_string(),
-            Token::From => FROM_KEYWORD.to_string(),
-            Token::GreaterThan => GREATER_THAN_SYMBOL.to_string(),
-            Token::GreaterThanOrEqual => GREATER_THAN_OR_EQUAL_SYMBOL.to_string(),
-            Token::IdentifierValue { value } => format!("\"{}\"", value),
-            Token::Index => INDEX_KEYWORD.to_string(),
-            Token::Insert => INSERT_KEYWORD.to_string(),
-            Token::Int => INT_KEYWORD.to_string(),
-            Token::Into => INTO_KEYWORD.to_string(),
-            Token::Key => KEY_KEYWORD.to_string(),
-            Token::LeftParenthesis => LEFT_PARENTHESIS_SYMBOL.to_string(),
-            Token::LessThan => LESS_THAN_SYMBOL.to_string(),
-            Token::LessThanOrEqual => LESS_THAN_OR_EQUAL_SYMBOL.to_string(),
-            Token::Minus => MINUS_SYMBOL.to_string(),
-            Token::NotEqual => NOT_EQUAL_SYMBOL.to_string(),
-            Token::Null => NULL_KEYWORD.to_string(),
-            Token::NumericValue { value } => value.to_string(),
-            Token::On => ON_KEYWORD.to_string(),
-            Token::Or => OR_KEYWORD.to_string(),
-            Token::Not => NOT_KEYWORD.to_string(),
-            Token::Plus => PLUS_SYMBOL.to_string(),
-            Token::Slash => SLASH_SYMBOL.to_string(),
-            Token::Modulo => MODULO_SYMBOL.to_string(),
-            Token::Exponentiation => EXPONENTIATION_SYMBOL.to_string(),
-            Token::SquareRoot => SQUARE_ROOT_SYMBOL.to_string(),
-            Token::CubeRoot => CUBE_ROOT_SYMBOL.to_string(),
-            Token::Factorial => FACTORIAL_SYMBOL.to_string(),
-            Token::FactorialPrefix => FACTORIAL_PREFIX_SYMBOL.to_string(),
-            Token::AbsoluteValue => ABS_SYMBOL.to_string(),
-            Token::BitwiseAnd => BITWISE_AND_SYMBOL.to_string(),
-            Token::BitwiseOr => BITWISE_OR_SYMBOL.to_string(),
-            Token::BitwiseXor => BITWISE_XOR_SYMBOL.to_string(),
-            Token::BitwiseNot => BITWISE_NOT_SYMBOL.to_string(),
-            Token::BitwiseShiftLeft => BITWISE_SHIFT_LEFT_SYMBOL.to_string(),
-            Token::BitwiseShiftRight => BITWISE_SHIFT_RIGHT_SYMBOL.to_string(),
-            Token::Primary => PRIMARY_KEYWORD.to_string(),
-            Token::RightParenthesis => RIGHT_PARENTHESIS_SYMBOL.to_string(),
-            Token::Select => SELECT_KEYWORD.to_string(),
-            Token::Semicolon => SEMICOLON_SYMBOL.to_string(),
-            Token::StringValue { value } => format!("'{}'", value),
-            Token::Table => TABLE_KEYWORD.to_string(),
-            Token::Text => TEXT_KEYWORD.to_string(),
-            Token::True => TRUE_KEYWORD.to_string(),
-            Token::Unique => UNIQUE_KEYWORD.to_string(),
-            Token::Values => VALUES_KEYWORD.to_string(),
-            Token::Where => WHERE_KEYWORD.to_string(),
-            Token::Alter => ALTER_KEYWORD.to_string(),
-            Token::Delete => DELETE_KEYWORD.to_string(),
-            Token::Update => UPDATE_KEYWORD.to_string(),
-            Token::Join => JOIN_KEYWORD.to_string(),
-            Token::Inner => INNER_KEYWORD.to_string(),
-            Token::Right => RIGHT_KEYWORD.to_string(),
-            Token::Left => LEFT_KEYWORD.to_string(),
-            Token::Constraint => CONSTRAINT_KEYWORD.to_string(),
-            Token::Foreign => FOREIGN_KEYWORD.to_string(),
-            Token::Double => DOUBLE_KEYWORD.to_string(),
-            Token::DoublePrecision => "DOUBLE PRECISION".to_string(),
-            Token::Precision => PRECISION_KEYWORD.to_string(),
-            Token::Real => REAL_KEYWORD.to_string(),
-            Token::SmallInt => SMALLINT_KEYWORD.to_string(),
-            Token::BigInt => BIGINT_KEYWORD.to_string(),
-            Token::Varchar => VARCHAR_KEYWORD.to_string(),
-            Token::Char => CHAR_KEYWORD.to_string(),
-            Token::Is => IS_KEYWORD.to_string(),
-            Token::TypeCast => TYPE_CAST_SYMBOL.to_string(),
-            Token::Distinct => DISTINCT_KEYWORD.to_string(),
-            Token::Order => ORDER_KEYWORD.to_string(),
-            Token::By => BY_KEYWORD.to_string(),
-            Token::OrderBy => "ORDER BY".to_string(),
-            Token::Asc => ASC_KEYWORD.to_string(),
-            Token::Desc => DESC_KEYWORD.to_string(),
-            Token::Limit => LIMIT_KEYWORD.to_string(),
-            Token::Offset => OFFSET_KEYWORD.to_string(),
-            Token::Dot => DOT_SYMBOL.to_string(),
-            Token::Outer => OUTER_KEYWORD.to_string(),
-            Token::Full => FULL_KEYWORD.to_string(),
-            Token::Like => LIKE_KEYWORD.to_string(),
-            Token::Comment => "".to_string(),
-        }
+        self.to_string()
     }
 }
 
